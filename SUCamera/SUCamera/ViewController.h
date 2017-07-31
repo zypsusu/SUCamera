0474e6fc41c9e62f81857644a5e17a25cfc5eaaf
zypsusu <zypsusu@zypsusudeMacBook-Air.local>
2017-07-31 14:54:38 +0800

    init

---
+++ SUCamera/SUCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SUCamera
+//
+//  Created by zypsusu on 2017/7/31.
+//  Copyright © 2017年 zypsusu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
